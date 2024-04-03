5f3fdb5fbdc562a7d0b41ecb5513a5c160c00bd7
Piolo Torrecampo <94138318+papz-i@users.noreply.github.com>
2024-04-03 22:09:14 +0800

    parameters object applied

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -9,14 +9,20 @@ using Unity.VisualScripting;
 
 public class RotateObject: MonoBehaviour
 {
-    public class PrefabRotate
+    public class Parameters
     {
-        public string action { get; set; }
-        public string prefab  { get; set; }
+        public string prefab { get; set; }
+        public float value { get; set; }
         public string axis { get; set; }
         public string object_to_replace { get; set; }
-        public float value { get; set; }
         public string direction { get; set; }
+    }
+    public class Prompt
+    {
+        public string prompt { get; set; }
+        public string action { get; set; }
+        public Parameters parameters { get; set; }
+
     }
 
     IEnumerator GetRequestRotate(string uri)
@@ -33,42 +39,42 @@ public class RotateObject: MonoBehaviour
                     Debug.LogError(String.Format("Something went wrong: {0}", webRequest.error));
                     break;
                 case UnityWebRequest.Result.Success:
-                    PrefabRotate prefab = JsonConvert.DeserializeObject<PrefabRotate>(webRequest.downloadHandler.text);
+                    Prompt prefab = JsonConvert.DeserializeObject<Prompt>(webRequest.downloadHandler.text);
                    
                     if (prefab.action == "rotate")
                     {
-                        GameObject objectPrefab = GameObject.Find(prefab.prefab);
-                        switch (prefab.axis.ToLower())
+                        GameObject objectPrefab = GameObject.Find(prefab.parameters.prefab);
+                        switch (prefab.parameters.axis.ToLower())
                         {
                             case "x":
-                                objectPrefab.transform.Rotate(prefab.value, 0.0f, 0.0f, Space.World); 
+                                objectPrefab.transform.Rotate(prefab.parameters.value, 0.0f, 0.0f, Space.World); 
                                 break;
                             case "y":
-                                objectPrefab.transform.Rotate(0.0f, prefab.value, 0.0f, Space.World);
+                                objectPrefab.transform.Rotate(0.0f, prefab.parameters.value, 0.0f, Space.World);
                                 break;
                             case "z":
-                                objectPrefab.transform.Rotate(0.0f, 0.0f, prefab.value, Space.World);
+                                objectPrefab.transform.Rotate(0.0f, 0.0f, prefab.parameters.value, Space.World);
                                 break;
                             default:
-                                objectPrefab.transform.Rotate(prefab.value, 0.0f, 0.0f, Space.World);
+                                objectPrefab.transform.Rotate(prefab.parameters.value, 0.0f, 0.0f, Space.World);
                                 break;
                         }
                     }else if (prefab.action == "scale")
                     {
-                        GameObject objectPrefab = GameObject.Find(prefab.prefab);
-                        switch (prefab.axis.ToLower())
+                        GameObject objectPrefab = GameObject.Find(prefab.parameters.prefab);
+                        switch (prefab.parameters.axis.ToLower())
                         {
                             case "x":
-                                objectPrefab.transform.localScale += new Vector3(prefab.value, 0, 0);
+                                objectPrefab.transform.localScale += new Vector3(prefab.parameters.value, 0, 0);
                                 break;
                             case "y":
-                                objectPrefab.transform.localScale += new Vector3(0, prefab.value, 0);
+                                objectPrefab.transform.localScale += new Vector3(0, prefab.parameters.value, 0);
                                 break;
                             case "z":
-                                objectPrefab.transform.localScale += new Vector3(0, 0, prefab.value);
+                                objectPrefab.transform.localScale += new Vector3(0, 0, prefab.parameters.value);
                                 break;
                             case "multiply":
-                                objectPrefab.transform.localScale += new Vector3(prefab.value, prefab.value, prefab.value);
+                                objectPrefab.transform.localScale += new Vector3(prefab.parameters.value, prefab.parameters.value, prefab.parameters.value);
                                 break;
                             case "increase":
                                 objectPrefab.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
@@ -82,10 +88,10 @@ public class RotateObject: MonoBehaviour
                         }
                     }else if(prefab.action == "remove")
                     {
-                        GameObject[] objectsToDestroy = GameObject.FindGameObjectsWithTag(prefab.prefab);
+                        GameObject[] objectsToDestroy = GameObject.FindGameObjectsWithTag(prefab.parameters.prefab);
                         if(objectsToDestroy.Length > 0)
                         {
-                            if(prefab.value > objectsToDestroy.Length)
+                            if(prefab.parameters.value > objectsToDestroy.Length)
                             {
                                 for (int i = 0; i < objectsToDestroy.Length; i++)
                                 {
@@ -94,7 +100,7 @@ public class RotateObject: MonoBehaviour
                             }
                             else
                             {
-                                for (int i = 0; i < prefab.value; i++)
+                                for (int i = 0; i < prefab.parameters.value; i++)
                                 {
                                     Destroy(objectsToDestroy[i]);
                                 }
@@ -109,7 +115,7 @@ public class RotateObject: MonoBehaviour
 
                         foreach (GameObject obj in objectsInScene)
                         {
-                            if (obj.name == prefab.object_to_replace)
+                            if (obj.name == prefab.parameters.object_to_replace)
                             {
                                 objectFound = true;
                                 objectPrefab = obj;
@@ -121,7 +127,7 @@ public class RotateObject: MonoBehaviour
                         {
                             Vector3 position = objectPrefab.transform.position;
                             Destroy(objectPrefab); 
-                            GameObject loadPrefab = Resources.Load<GameObject>(prefab.prefab);
+                            GameObject loadPrefab = Resources.Load<GameObject>(prefab.parameters.prefab);
                             GameObject prefabInstance = Instantiate(loadPrefab, position, Quaternion.identity);
                         }
                         else
@@ -145,25 +151,25 @@ public class RotateObject: MonoBehaviour
 
                             Debug.Log("Left: " + left + ", Right: " + right + ", Top: " + top + ", Bottom: " + bottom);
                             
-                            switch (prefab.direction)
+                            switch (prefab.parameters.direction)
                             {
                                 case "left":
-                                    objectPrefab.transform.Translate(Vector3.left * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.left * prefab.parameters.value);
                                     break;
                                 case "right":
-                                    objectPrefab.transform.Translate(Vector3.right * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.right * prefab.parameters.value);
                                     break;
                                 case "top":
-                                    objectPrefab.transform.Translate(Vector3.up * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.up * prefab.parameters.value);
                                     break;
                                 case "bottom":
-                                    objectPrefab.transform.Translate(Vector3.down * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.down * prefab.parameters.value);
                                     break;
                                 case "back":
-                                    objectPrefab.transform.Translate(Vector3.back * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.back * prefab.parameters.value);
                                     break;
                                 case "front":
-                                    objectPrefab.transform.Translate(Vector3.forward * prefab.value);
+                                    objectPrefab.transform.Translate(Vector3.forward * prefab.parameters.value);
                                     break;
                                 default:
                                     break;
@@ -174,7 +180,7 @@ public class RotateObject: MonoBehaviour
                             Debug.LogError("Renderer component not found on the prefab.");
                         }
                     }
-                    Debug.Log("Success | Object Name: " + prefab.prefab);
+                    Debug.Log("Success | Object Name: " + prefab.parameters.prefab);
                 break;
                    
             }
@@ -185,7 +191,7 @@ public class RotateObject: MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(GetRequestRotate("http://127.0.0.1:5000/rotate"));
+            StartCoroutine(GetRequestRotate("http://127.0.0.1:5000/move"));
         }
     }
 }
